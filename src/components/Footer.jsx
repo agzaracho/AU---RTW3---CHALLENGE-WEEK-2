0e45d2b72997cf92bf0efa0adde1f70ac01d72ca
Aquiles Gonzalez <aquilesdgonzalez@gmail.com>
2023-07-31 21:20:40 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
       <div className="text-center mt-2 p-4">
        
           <a className="text-lime-400" href="https://university.alchemy.com/"> Alchemy University | </a> 
-          {account ? (<>If you bought me a coffee. Ask me for a thank you poap on Discord. Thank you!</>):(<>© 2022 - Front for the smart contract develop in the RTW3 - Week 2 </>)}
+          {account ? (<>If you bought me a coffee, ask me for a thank you POAP on Discord. Thank you!</>):(<>Front for dApp develop in the RTW3</>)}
       </div>
     </footer>
 
